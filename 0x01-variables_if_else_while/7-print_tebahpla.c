94695811ca5b21f186cd7440b2ef186ae76ab24b
Colezzzer <lethabomohlaloga4@gmail.com>
2023-07-14 11:41:31 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - This project prints the lowercase alphabet in reverse, followed by a new line
- * 
+ * main -  prints the lowercase alphabet in reverse,
+ * followed by a new line 
  * Return: Always 0 (Success)
  */
 int main(void)
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - This project prints numbers between 0 to 9 and letters between a to f.
- *
+ * main - Prints numbers between 0 to 9 and letters between a to f.
  * Return: Always 0 (Success)
  */
 int main(void)
